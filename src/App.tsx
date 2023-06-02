fe835b119d89f26b6a4681952fd009f71d377370
dmitry-sharashkin <faustfbs@mail.ru>
2023-06-02 10:11:43 +0300

    add mui and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import SignIn from "./pages/SignIn/SignIn";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Routes>
+        <Route element={<SignIn />} path="/auth" />
+      </Routes>
+    </>
   );
 }
 
